806fa04798f99bb4c4914c3c238e915dbdd29d8e
reisleo99 <leonardo.ao.reis@gmail.com>
2024-09-03 16:59:37 -0400

    Adjustments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
-import './footer.css';
+import './Footer.css';
 
 const Footer = () => {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './header.css';
+import './Header.css';
 import profilePic from '../assets/perfil-profissional.jpg';
 
 const Header = () => {
