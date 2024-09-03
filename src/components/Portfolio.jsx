a6f58a1b78d8d53b6423f91005d55538d62b1dd1
reisleo99 <leonardo.ao.reis@gmail.com>
2024-09-03 17:48:17 -0400

    Added images to portfolio projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,16 +7,16 @@ const Portfolio = () => {
       <h1>My Projects</h1>
       <div className="portfolio-grid">
         <a href="https://github.com/Reisleo99/TimeSheetJuly2nd" target="_blank" rel="noopener noreferrer">
-          <img src="/path/to/timesheet-image.jpg" alt="TimeSheet Project" className="portfolio-item" />
+          <img src="../../public/Timesheet.png" alt="TimeSheet Project" className="portfolio-item" />
         </a>
         <a href="https://github.com/Reisleo99/Professional-ReadME-Generator" target="_blank" rel="noopener noreferrer">
-          <img src="/path/to/readme-generator-image.jpg" alt="ReadME Generator Project" className="portfolio-item" />
+          <img src="../../public/Readme.png" alt="ReadME Generator Project" className="portfolio-item" />
         </a>
         <a href="https://github.com/Reisleo99/vehicle-management-platform" target="_blank" rel="noopener noreferrer">
-          <img src="/path/to/vehicle-management-image.jpg" alt="Vehicle Management Project" className="portfolio-item" />
+          <img src="../../public/vehiclemanagement.png" alt="Vehicle Management Project" className="portfolio-item" />
         </a>
         <a href="https://github.com/Reisleo99/Employee-Management-Platform" target="_blank" rel="noopener noreferrer">
-          <img src="/path/to/employee-management-image.jpg" alt="Employee Management Project" className="portfolio-item" />
+          <img src="../../public/employeemanagement.png" alt="Employee Management Project" className="portfolio-item" />
         </a>
       </div>
     </div>
@@ -25,6 +25,3 @@ const Portfolio = () => {
 
 export default Portfolio;
 
-//TODO Deixar imagens com links para os projetos no GitHub
-//TODO Deploy no netlify
-//TODO Link para baixar resume
