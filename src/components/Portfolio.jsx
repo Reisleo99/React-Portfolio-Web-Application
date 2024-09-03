a05b6ed7670020e6b5adcae894d6b22bb8ed582b
reisleo99 <leonardo.ao.reis@gmail.com>
2024-09-03 17:51:42 -0400

    Adjustments

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,16 +7,16 @@ const Portfolio = () => {
       <h1>My Projects</h1>
       <div className="portfolio-grid">
         <a href="https://github.com/Reisleo99/TimeSheetJuly2nd" target="_blank" rel="noopener noreferrer">
-          <img src="../../public/Timesheet.png" alt="TimeSheet Project" className="portfolio-item" />
+          <img src="../assets/Timesheet.png" alt="TimeSheet Project" className="portfolio-item" />
         </a>
         <a href="https://github.com/Reisleo99/Professional-ReadME-Generator" target="_blank" rel="noopener noreferrer">
-          <img src="../../public/Readme.png" alt="ReadME Generator Project" className="portfolio-item" />
+          <img src="../assets/Readme.png" alt="ReadME Generator Project" className="portfolio-item" />
         </a>
         <a href="https://github.com/Reisleo99/vehicle-management-platform" target="_blank" rel="noopener noreferrer">
-          <img src="../../public/vehiclemanagement.png" alt="Vehicle Management Project" className="portfolio-item" />
+          <img src="../assets/vehiclemanagement.png" alt="Vehicle Management Project" className="portfolio-item" />
         </a>
         <a href="https://github.com/Reisleo99/Employee-Management-Platform" target="_blank" rel="noopener noreferrer">
-          <img src="../../public/employeemanagement.png" alt="Employee Management Project" className="portfolio-item" />
+          <img src="../assets/employeemanagement.png" alt="Employee Management Project" className="portfolio-item" />
         </a>
       </div>
     </div>
